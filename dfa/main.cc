2c6ecda3bd0a1206fc61f863ccb50c7acebc3fa7
Antony Kellermann <aokellermann@gmail.com>
2020-05-30 12:25:43 -0400

    Add empty executable dfash

---
+++ dfa/main.cc
@@ -0,0 +1,14 @@
+/**
+ * @file main.cc
+ * @author Antony Kellermann
+ * @copyright 2020 Antony Kellermann
+ */
+
+#include "dfa/dfa.h"
+
+#include <iostream>
+
+int main(int argc, char** argv)
+{
+  return 0;
+}
